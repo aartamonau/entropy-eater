0acf6e3d078355267b5b689e504684f925ce57df
Aliaksiej Artamonaŭ <aliaksiej.artamonau@gmail.com>
2010-09-20 23:07:18 +0300

    FSM doxygen comments.

--- src/module/eater_fsm.c
+++ src/module/eater_fsm.c
@@ -4,26 +4,36 @@
 #include "eater_fsm.h"
 
 
+/// Event handler taking event-specific data.
 typedef int (*eater_fsm_event_handler_t)(void *);
+
+
+/// Event handler that does not take event-specific data.
 typedef int (*eater_fsm_event_handler_no_data_t)(void);
 
 
+/// Event handler type.
 enum eater_fsm_event_handler_type_t {
-  EATER_FSM_EVENT_HANDLER_NO_DATA,
-  EATER_FSM_EVENT_HANDLER_WITH_DATA
+  EATER_FSM_EVENT_HANDLER_NO_DATA,    /**< Handler without arguments. */
+  EATER_FSM_EVENT_HANDLER_WITH_DATA   /**< Handler taking argument. */
 };
 
+
+/// Event handler.
 struct eater_fsm_event_handler_t {
-  enum eater_fsm_event_handler_type_t type;
+  enum eater_fsm_event_handler_type_t type; /**< Handler type. */
 
   union {
+    /// Handler without data.
     struct {
-      int (*fn)(void);
+      int (*fn)(void);          /**< Function to be called. */
     } no_data;
 
+    /// Handler with data.
     struct {
-      ptrdiff_t offset;
-      int (*fn)(void *);
+      ptrdiff_t offset;         /**< Offset of handlers data in
+                                 * #eater_fsm_event_data_t union. */
+      int (*fn)(void *);        /**< Function to be called. */
     } with_data;
   } h;
 };
@@ -37,45 +47,87 @@ struct eater_fsm_event_handler_t {
     }                                                       \
   }                                                         \
 
+
 #define _EVENT_HANDLER_NO_DATA(_handler) { .no_data = { _handler } }
 
+
+/**
+ * Static initializer for handler taking data argument.
+ *
+ * @param _type        type of event that triggers this handler
+ * @param _handler     function to be called when event arrives
+ * @param _data_member name of the event-specific data in
+ *                     #eater_fsm_event_data_t union
+ */
 #define EVENT(_type, _handler, _data_member)                   \
   [_type] = { .type = EATER_FSM_EVENT_HANDLER_WITH_DATA,       \
               .h    = _EVENT_HANDLER(_handler, _data_member) }
 
+
+/**
+ * Static initializer for handler that does not take data argument.
+ *
+ * @param _type    type of event that triggers this handler
+ * @param _handler function to be called when event arrives
+ */
 #define EVENT_NO_DATA(_type, _handler)                          \
   [_type] = { .type = EATER_FSM_EVENT_HANDLER_NO_DATA,          \
               .h    = _EVENT_HANDLER_NO_DATA(_handler) }
 
 
+/**
+ * Sample handler.
+ *
+ * @retval EATER_FSM_STATE_IDLE
+ */
 int
 eater_fsm_hello_handler(void);
 
 
+/// Event handlers.
 static const
 struct eater_fsm_event_handler_t handlers[EATER_FSM_EVENT_TYPES_COUNT] = {
   EVENT_NO_DATA(EATER_FSM_EVENT_TYPE_HELLO, eater_fsm_hello_handler),
 };
 
 
+/**
+ * Dispatches generic event to specific handlers.
+ *
+ * @param event event to dispatch
+ *
+ * @retval >=0 new state of FSM
+ * @retval  <0 error code
+ */
 int
 eater_fsm_event_dispatch(struct eater_fsm_event_t *event);
 
 
+/// FSM context.
 struct eater_fsm_t {
-  enum eater_fsm_state_t state;
+  enum eater_fsm_state_t state; /**< Current state. */
 };
 
 
+/// FSM.
 static struct eater_fsm_t fsm = { .state = EATER_FSM_STATE_IDLE };
 
 
 /* Auxiliary functions and macros */
 
+
+/// Checks whether event seems to be valid. If not BUG()s.
 #define ASSERT_VALID_EVENT(event) \
   ASSERT( (event)->type < EATER_FSM_EVENT_TYPES_COUNT )
 
 
+/**
+ * Converts event types to strings.
+ *
+ * @param type event type
+ *
+ * @return string representation of event type
+ */
 const char *
 eater_fsm_event_type_to_str(enum eater_fsm_event_type_t type);
 
--- src/module/eater_fsm.h
+++ src/module/eater_fsm.h
@@ -2,33 +2,49 @@
 #define _EATER_FSM_H_
 
 
+/// Eater's FSM states.
 enum eater_fsm_state_t {
   EATER_FSM_STATE_IDLE,
   __EATER_FSM_STATE_LAST
 };
 
+
+/// Number of states in eater's FSM.
 #define EATER_FSM_STATES_COUNT __EATER_FSM_STATE_LAST
 
 
+/// Event types that eater's FSM can handle.
 enum eater_fsm_event_type_t {
   EATER_FSM_EVENT_TYPE_HELLO,
   __EATER_FSM_EVENT_TYPE_LAST
 };
 
+
+/// Number of event types.
 #define EATER_FSM_EVENT_TYPES_COUNT __EATER_FSM_EVENT_TYPE_LAST
 
 
+/// Event specific data encapsulated in a single union.
 union eater_fsm_event_data_t {
   int data;
 };
 
-struct eater_fsm_event_t {
-  enum eater_fsm_event_type_t type;
 
-  union eater_fsm_event_data_t data;
+/// Event.
+struct eater_fsm_event_t {
+  enum eater_fsm_event_type_t  type; /**< Event type. */
+  union eater_fsm_event_data_t data; /**< Event data. */
 };
 
 
+/**
+ * Feeds event to FSM.
+ *
+ * @param event event
+ *
+ * @retval  0 success
+ * @retval <0 error code
+ */
 int
 eater_fsm_emit(struct eater_fsm_event_t *event);
 
